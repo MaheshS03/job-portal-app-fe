23f687ccae64e993d74e433ce6420e98246c5ace
MaheshS03 <maheshraina375@gmail.com>
2024-05-16 15:23:06 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,27 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import GetAllBooksComponent from './components/GetAllBooksComponent/GetAllBooksComponent';
-import AddNewBooksComponent from './components/AddNewBooksComponent/AddNewBooksComponent';
-import EditBooksComponent from './components/EditBooksComponent/EditBooksComponent';
+import GetAllJobsComponent from './components/GetAllJobsComponent/GetAllJobsComponent';
+import AddNewJobsComponent from './components/AddNewJobsComponent/AddNewJobsComponent';
+import EditJobsComponent from './components/EditJobsComponent/EditJobsComponent';
+import DeleteJobsComponent from './components/DeleteBooksComponent/DeleteJobsComponent';
 
 function App() {
   return (
     <Router>
             <div className="container">
-              <h1>Library App</h1>
+              <h1>Job Portal App</h1>
               
             <nav className="nav-menu">
                 <Link to="/" >Home</Link>
-                <Link to="/admin/add" >Add Books</Link>
-                <Link to="/admin/edit" >Edit Books</Link>
+                <Link to="/admin/add" >Add Jobs</Link>
+                <Link to="/admin/edit" >Edit Jobs</Link>
+                <Link to="/admin/delete" >Delete Jobs</Link>
             </nav>
            <Routes>
-                 <Route exact path='/' element={<GetAllBooksComponent/>}></Route>
-                 <Route path='/admin/add' element={<AddNewBooksComponent/>}></Route>
-                 <Route path='/admin/edit' element={<EditBooksComponent/>}></Route>
+                 <Route exact path='/' element={<GetAllJobsComponent/>}></Route>
+                 <Route path='/admin/add' element={<AddNewJobsComponent/>}></Route>
+                 <Route path='/admin/edit' element={<EditJobsComponent/>}></Route>
+                 <Route path='/admin/delete' element={<DeleteJobsComponent/>}></Route>
           </Routes>
           </div>
        </Router>
